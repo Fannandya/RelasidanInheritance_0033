76e0b7c97e4a9948f1065a5c6f6741663e1f3a2f
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-05-26 17:54:54 +0700

    membuat class orang dan constructor destructor

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class orang
+{
+public:
+    string nama;
+
+    orang(string pNama) : nama(pNama)
+    {
+        cout << "orang dibuat\n"
+             << endl;
+    }
+    ~orang()
+    {
+        cout << "orang dihapus\n"
+             << endl;
+    }
+}
